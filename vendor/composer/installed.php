0b6f5bf08de52061f9dc53ff0b537526eb4ecb19
lefty993 <antoniovinkovic1@gmail.com>
2020-11-10 20:43:08 +0100

    novi update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9ddaae23cadfbfc1ae236a25fd114285683b3ca0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9ddaae23cadfbfc1ae236a25fd114285683b3ca0',
+    ),
+    'ondram/ci-detector' => 
+    array (
+      'pretty_version' => '3.5.1',
+      'version' => '3.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '594e61252843b68998bddd48078c5058fe9028bd',
+    ),
+  ),
+);
